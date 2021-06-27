9b5e5c0c458e0b3a83ccb4b84317756469c1771d
edmund <edmund.gichogo@student.moringaschool>
2021-06-27 21:47:00 +0300

    make first test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function Order(){
     this.items = {}
-    this.size = Object.keys(this.items).length
+    this.size = 0
     this.total = 0
 }
 function Item(price,type,id){
@@ -14,10 +14,15 @@ function OrderItem(id,quantity,price){
     this.price = price
 }
 Order.prototype.addItem = function(item){
+    console.log(item.id,item.price)
     this.items = this.items || {}
-    let currentItem = this.items[item.id] || new OrderItem(item.id,0,item.price)
-    currentItem.quantity = currentItem ++
+    let currentItem = this.items[item.id] || new OrderItem(item.id,1,item.price)
     this.items[item.id] = currentItem
+    if(this.size === 0){
+        this.size = 1
+    $('#size').val(1)
+
+    }
 }
 Order.prototype.getTotal = function(){
     var total = 0
@@ -27,16 +32,29 @@ Order.prototype.getTotal = function(){
         total = total + currentTotal 
     }
     this.total = total
+    $('#totalCost').text(total)
         return total
 }
+var order = new Order()
 $(document).ready(function(){
     $( ".clickable").on('click',function(e){
-        console.log(e)
-        console.log(e.target.id)
-        console.log($('#' + e.target.id).data())
-
+        var properties = $('#' + e.target.id).data()
         var itemID = e.target.id
-        var itemData = $('#' + itemID)
+        var itemData = $('#' + itemID).data()
+        var item = new Item(itemData.price,itemData.type,itemID)
+        order.addItem(item)
+        console.log(111,item)
+        console.log(222,order)
+        console.log(333,order.getTotal())
+    })
+    $('#size').on('change',function(e){
+        var value = e.target.value
+        if(Number.isInteger(value) && value>0){
+            order.size = value
+            order.getTotal()
+        }else{
+            alert("Invalid input")
+        }
     })
 })
 
